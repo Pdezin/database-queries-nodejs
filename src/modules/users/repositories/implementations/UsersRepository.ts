d04e77e93ed46f22c20968f2235e55b9335e5a9e
Pedro Jardim <pedro200219@outlook.com>
2022-06-11 15:18:44 -0300

    Finalized :sparkles:

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -14,17 +14,26 @@ export class UsersRepository implements IUsersRepository {
   async findUserWithGamesById({
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
+    const user = await this.repository.findOne({id: user_id}, {relations: ["games"]})
+
+    if (!user) {
+      throw new Error("User not found")
+    }
+
+    return user;
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query("SELECT * FROM users ORDER BY users.first_name");
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    const firstName = first_name.toLowerCase()
+    const lastName = last_name.toLowerCase()
+
+    return this.repository.query("SELECT * FROM users WHERE LOWER(users.first_name) = $1 AND LOWER(users.last_name) = $2", [firstName, lastName]); 
   }
 }
